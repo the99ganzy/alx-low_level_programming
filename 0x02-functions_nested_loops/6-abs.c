954df781b5560652a5c0cef6e83923b86cad4491
root <lizzyganzy@gmroot@4ffa91e3d00d:/alx-pre_course# git config --global user.email lizzyganzy@g>
2022-03-16 16:34:19 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
